80ef1c634876efa1be97922c60e0c371a60e6383
josephine03 <57975730+josephine03@users.noreply.github.com>
2019-11-21 11:59:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,12 +9,13 @@
 	<?php
 	#Second comment sample
 	?>
-	<a href="welcome.php">welcome </a>
+	
     <p> Choose a page you desire </p><br>
 	<a href="welcome.php"><button> welcome</button></a><br>
 	<a href="displaymethod.php"><button>display method</button></a><br>
      <a href="getpost.php"><button>getpost</button></a><br>
       <a href="getmethod"><button>getmethod</button></a><br>
+	<a href="welcome.php">welcome </a>
 
 </body>
 </html>
