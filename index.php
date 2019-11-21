58bf8e7956e32de4e54ff67c798fb874e47b668b
josephine03 <57975730+josephine03@users.noreply.github.com>
2019-11-21 11:48:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,17 @@
 	<title>Hello World</title>
 </head>
 <body>
-	<?php  echo "Hello,  World!";?>
-	<a href="welcome.php">home </a>
+	<?php  #first sample comment
+	echo "Hello,World!";?><br>
+	<?php
+	#Second comment sample
+	?>
+	<a href="welcome.php">welcome </a>
+    <p> Choose a page you desire </p><br>
+	<a href="welcome.php"><button> welcome</button></a><br>
+	<a href="displaymethod.php"><button>display method</button></a><br>
+     <a href="getpost.php"><button>getpost</button></a><br>
+      <a href="getmethod"><button>getmethod</button></a><br>
+
 </body>
 </html>
